30501af55490453935ff067ea843f09251962431
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-08-16 16:53:17 +0100

    refactoring

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,38 @@
-export type FlexFunc<T> = (() => T) | (() => Promise<T>);
+export type FlexFunc<T> = (() => T | Promise<T>);
 
 export type FlexValue<T> = T | FlexFunc<T>;
 
 export interface FlexOptions<T> {
-    onError: (err: any) => never | PromiseLike<T>
+    /**
+     * Optional error handler, which can also a value
+     * to be used in case of an error.
+     */
+    onError?: (err: any) => T | undefined
+
+    /**
+     * Calling Context in case of a function.
+     */
+    cc?: any
 }
 
-export async function flexValue<T>(value: FlexValue<T>, options?: FlexOptions<T>): Promise<T | undefined> {
+export async function flexValue<T>(value: FlexValue<T>, options?: FlexOptions<T>): Promise<T> {
     if (typeof value === 'function') {
-        let onError: null | ((err: any) => any) = null;
-        if (options && typeof options.onError === 'function') {
-            onError = options.onError;
-        }
-        try {
-            const v = (<FlexFunc<T>>value)();
-            const p = <Promise<T>>v;
-            if (p && typeof p.catch === 'function') {
-                if (onError) {
-                    return await p.catch<T>(onError);
-                } else {
-                    return p;
+        const onError = options && typeof options.onError === 'function' ? options.onError : null;
+        const cc = options && options.cc;
+        if (onError) {
+            try {
+                const p = <Promise<T>>(<FlexFunc<T>>value).call(cc);
+                if (p && typeof p.catch === 'function') {
+                    return await p.catch<T>(err => {
+                        return <T>onError(err);
+                    });
                 }
+                return p;
+            } catch (e) {
+                return <T>onError(e);
             }
-            return v;
-        } catch (e) {
-            if (onError) {
-                onError(e);
-            } else {
-                throw e;
-            }
+        } else {
+            return (<FlexFunc<T>>value).call(cc);
         }
     } else {
         return value;
