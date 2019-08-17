bedd0954e17e2cc32f6e0d555bb7fc5cfd4ac986
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2019-08-17 17:44:18 +0100

    adding tests

--- test/get.spec.ts
+++ test/get.spec.ts
@@ -27,20 +27,27 @@ describe('get', () => {
             expect(v).to.eq(456);
         });
         it('must pass in the error correctly', async () => {
-            let err;
+            let err, name;
             const v = await Flex.get<number>(Promise.reject(123), {
-                onError: e => {
+                onError: (e: any, n?: string) => {
                     err = e;
-                }
+                    name = n;
+                }, name: 'hello'
             });
             expect(err).to.eq(123);
+            expect(name).to.eq('hello');
             expect(v).to.be.undefined;
         });
     });
     describe('for value-returning callbacks', () => {
         it('must return the value', async () => {
             const a: string = await Flex.get(() => 'tst');
+            const b: number = await Flex.get(() => 123, {
+                onError: () => {
+                }
+            });
             expect(a).to.eq('tst');
+            expect(b).to.eq(123);
         });
         it('must reject on errors', async () => {
             let err: any;
@@ -66,6 +73,31 @@ describe('get', () => {
         });
     });
     describe('for promise-returning callbacks', () => {
-
+        it('must return the value', async () => {
+            const a: string = await Flex.get(() => Promise.resolve('tst'));
+            expect(a).to.eq('tst');
+        });
+        it('must reject on errors', async () => {
+            let err: any;
+            await Flex.get(() => {
+                return Promise.reject('ops!');
+            }).catch(e => {
+                err = e;
+            });
+            expect(err).to.eq('ops!');
+        });
+        it('must redirect errors', async () => {
+            let err1: any, err2: any;
+            const onError = (e: any) => {
+                err1 = e;
+            };
+            await Flex.get(() => {
+                return Promise.reject('ops!');
+            }, {onError}).catch(e => {
+                err2 = e;
+            });
+            expect(err1).to.eq('ops!');
+            expect(err2).to.be.undefined;
+        });
     });
 });
